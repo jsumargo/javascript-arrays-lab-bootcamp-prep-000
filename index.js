a18c3df738632ecd1468000f1f4865e81f975adf
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-04 10:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ function prependKitten(name) {
   var newArray = [name, ...kittens];
   return newArray;
 }
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1);
+  return newArray;
+}
